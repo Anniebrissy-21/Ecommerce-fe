813272a6987f7c57baa15734881f7e80990f4040
Annie <annie.b@scalgo.net>
2025-06-09 16:23:53 +0530

    detail page and auto generation of code

--- src/components/product/ProductPage.jsx
+++ src/components/product/ProductPage.jsx
@@ -1,20 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductPlaceholder from './ProductPlaceholder'
+import { useParams } from 'react-router-dom'
+import api from '../../api'
+import RelatedProducts from './RelatedProducts'
+import { BASE_URL } from '../../api'
 
 const ProductPage = () => {
+
+    const { slug } = useParams()
+    const [product, setProduct] = useState({})
+    const [similarProducts, setSimilarProducts] = useState([])
+    const [loading, setloading] = useState(false)
+
+    // function add_item() {
+    //     api.post("add_item")
+    // }
+
+    useEffect(function () {
+        setloading(true)
+        api.get(`product_detail/${slug}`).then(res => {
+            console.log(res.data)
+            setProduct(res.data)
+            setSimilarProducts(res.data.similar_products)
+            setloading(false)
+        })
+            .catch(err => {
+                console.log(err.message)
+                setloading(false)
+            })
+    }, [slug])
+
+    if (loading) {
+        return <ProductPlaceholder />;
+    }
+
     return (
         <div>
             <section className='py-3'>
                 <div className='container px-4 px-lg-5 my-5'>
                     <div className='row gx-4 gx-lg-5 align-items-center'>
                         <div className="col-md-6">
-                            <img className='card-img-top mb-5 mb-md-0' src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." />
+                            <img className='card-img-top mb-5 mb-md-0' src={`${BASE_URL}${product.image}`} alt="..." />
                         </div>
                         <div className='col-md-6'>
                             <div className="small mb-1"> SKU: BST-496</div>
-                            <h1 className='display-5 fw-bolder'>Shop item template</h1>
+                            <h1 className='display-5 fw-bolder'>{product.name}</h1>
                             <div className="fs-5 mb-5">
-                                <span className='text-decoration-line-through'>$40.00</span>
-                                <span>$40.00</span>
+                                {/* <span className='text-decoration-line-through'>$40.00</span> */}
+                                <span>{`$${product.price}`}</span>
                             </div>
                             <p className='lead'>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quis esse velit,
@@ -24,8 +57,8 @@ const ProductPage = () => {
                                 Fugit sed sequi, animi aperiam odio vero eos.
                             </p>
                             <div className="d-flex">
-                                <input type="num" className='form-control text-center me-3'
-                                    id='inputQuantity' value='1' style={{ maxWidth: '3rem' }} />
+                                {/* <input type="num" className='form-control text-center me-3'
+                                    id='inputQuantity' value='1' style={{ maxWidth: '3rem' }} /> */}
 
                                 <button className='btn btn-outline-dark flex-shrink-0' type='button'>
                                     <i className='bi-cart-fill me-1'></i>
@@ -39,6 +72,7 @@ const ProductPage = () => {
 
 
             </section>
+            <RelatedProducts products={similarProducts} />
         </div>
     )
 }
